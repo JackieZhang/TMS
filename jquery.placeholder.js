a27b3e9c5de97c9e5942f2625f07a93bb3f77143
JackieZhang <jackiedhu@163.com>
2014-01-25 10:46:07 +0800

    placeholder for html <5

---
+++ jquery.placeholder.js
@@ -0,0 +1,23 @@
+
+
+function placeholder(select, holder){
+$(select).val(holder);
+
+$(select).focus(function(){
+if($(select).val() == holder){
+$(select).val('');
+}
+});
+
+$(select).blur(function(){
+if($(select).val() == ''){
+$(select).val(holder);
+}
+});
+
+}
+
+
+for (var t in $("[placeholder]")){
+placeholder("[placeholder]:eq("+t+")", $("[placeholder]:eq("+t+")").attr('placeholder'));
+}
